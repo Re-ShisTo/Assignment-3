6c02b9198cf16d354a12fc0009b95b515b817f78
Mango God <anil7053r@gmail.com>
2025-11-24 22:44:02 +0600

    onclick navigate added to nav logo

--- src/Component/Nav bar/NavBar.jsx
+++ src/Component/Nav bar/NavBar.jsx
@@ -2,7 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./NavBar.css";
 import S_logo_black from "../../assets/S-logo-black.png";
 import S_logo from "../../assets/S-logo.png";
-import { Link, NavLink, useLocation } from "react-router-dom";
+import {
+  Link,
+  Navigate,
+  NavLink,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 
 const NavBar = () => {
   //useLocation for about section
@@ -11,6 +17,8 @@ const NavBar = () => {
   const [menuActive, setMenuActive] = useState(false);
   //menubar for smaller screen
   const [menuBar, setMenuBar] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (hash) {
@@ -23,7 +31,7 @@ const NavBar = () => {
 
   return (
     <div className="flex-box nav" id="top">
-      <picture className="logo">
+      <picture className="logo" onClick={() => navigate("/")}>
         <source srcSet={S_logo} media="(max-width: 768px)" />
         <img src={S_logo_black} alt="" />
       </picture>
